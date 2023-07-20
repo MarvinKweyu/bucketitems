f4a2cf0719953d6107fb2e89044fa5d449f69359
MarvinKweyu <mkweyu1@gmail.com>
2023-07-20 08:41:47 +0300

    chore: display note item

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:bucketitems/models/notes.dart';
+import 'package:bucketitems/models/note.dart';
 
 void main() {
   runApp(const MyApp());
@@ -67,16 +68,15 @@ class _MyHomePageState extends State<MyHomePage> {
                     itemBuilder: (context, index) {
                       return GestureDetector(
                           onTap: () {
-                            print("Item at index $index tapped");
                             // item id
-                            print(snapshot.data!.notes[index].id.toString());
+
                             Navigator.push(
                                 context,
                                 MaterialPageRoute(
                                     builder: (context) => NotePage(
                                         title: 'Note',
-                                        noteId: snapshot.data!.notes[index].id
-                                            .toString())));
+                                        noteId:
+                                            snapshot.data!.notes[index].id)));
                           },
                           child: Column(
                             children: <Widget>[
@@ -121,18 +121,44 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class NotePage extends StatelessWidget {
+class NotePage extends StatefulWidget {
   const NotePage({super.key, required this.title, required this.noteId});
 
   final String title;
-  final String noteId;
+  final int noteId;
+
+  @override
+  State<NotePage> createState() => _NotePageState();
+}
+
+class _NotePageState extends State<NotePage> {
+  late Future<Note> futureNote;
+
+  @override
+  void initState() {
+    super.initState();
+    // get the note with the id
+    futureNote = getNote(widget.noteId);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: const Center(
-        child: Text('Note Page'),
+      appBar: AppBar(title: Text(widget.title)),
+      body: Center(
+        child: Center(
+            child: FutureBuilder<Note>(
+          future: futureNote,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(snapshot.data!.title);
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+            // show default spinner by default
+            return const CircularProgressIndicator();
+          },
+        )),
       ),
     );
   }
@@ -208,8 +234,18 @@ Future<Notes> getNotes() async {
       await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
 
   if (response.statusCode == 200) {
-    print(response.body);
     return Notes.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to load note');
+  }
+}
+
+Future<Note> getNote(int noteId) async {
+  final response = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/posts/$noteId'));
+
+  if (response.statusCode == 200) {
+    return Note.fromJson(jsonDecode(response.body));
   } else {
     throw Exception('Failed to load note');
   }
